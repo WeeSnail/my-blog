384788c0f6bdfa8c1edd9e5f90ea99d9f7ba05cb
Anes-Marzooki <marzoukianas@gmail.com>
2021-10-20 18:44:23 +0100

    Configure manifest file's options

---
+++ utils/manifest-options.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Anes\'s blog',
+    short_name: 'Anes\'s blog',
+    start_url: '/',
+    background_color: '#f7f0eb',
+    theme_color: '#a2466c',
+    display: 'standalone',
+    icon: `src/images/icon.png`,
+}
