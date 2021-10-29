33eaf2dec25ffd445bd5390dbfe4814b32f38d5c
Anes-Marzooki <marzoukianas@gmail.com>
2021-10-29 02:44:16 +0100

    Indentation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 import {Link} from 'gatsby';
 
 const style = {
-  time: {
-    fontSize: "12px"
-  }
+    time: {
+        fontSize: "12px"
+    }
 }
 
 const Post = ({title, date, subtitle, slug}) => {
-  return (
-    <>
-      <div className="content is-normal">
-        <div className="head-wrapper mb-2">
-          <h2 className="mb-0">{title}</h2>
-          <time
-            style={style.time}
-            dateTime={style.time}>{date}</time>
-        </div>
-        <p>{subtitle}</p>
-        <Link
-          className="button is-link is-light is-small"
-          to={`posts/${slug}`}>
-          Continue reading
-        </Link>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="content is-normal">
+                <div className="head-wrapper mb-2">
+                    <h2 className="mb-0 post-title">{title}</h2>
+                    <time
+                        style={style.time}
+                        dateTime={style.time}>{date}</time>
+                </div>
+                <p>{subtitle}</p>
+                <Link
+                    className="button is-link is-light"
+                    to={`posts/${slug}`}>
+                    Continue reading
+                </Link>
+            </div>
+        </>
+    )
 };
 
 export default Post;
