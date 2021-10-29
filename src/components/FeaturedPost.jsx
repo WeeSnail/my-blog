64503dbbd505c8161f423f65c451b6daf625c63a
Anes-Marzooki <marzoukianas@gmail.com>
2021-10-29 02:43:01 +0100

    Remove redundant code

--- src/components/FeaturedPost.jsx
+++ src/components/FeaturedPost.jsx
@@ -2,34 +2,42 @@ import React from 'react';
 import {Link} from 'gatsby';
 
 const style = {
-  time: {
-    fontSize: "12px"
-  }
+    card: {
+        marginTop: "100px"
+    },
+    time: {
+        fontSize: "12px"
+    }
 };
 
 const FeaturedPost = ({post}) => {
-  return (
-    <div className="card">
-      <div className="card-content">
-        <div className="media">
-          <div className="media-content">
-            <p className="title is-4">{post.frontmatter.title}</p>
-            <p className="subtitle is-6">{post.frontmatter.author}</p>
-          </div>
+
+    return (
+        <div className="card" syle={style.card}>
+            <div className="card-content">
+
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{post.frontmatter.title}</p>
+                        <p className="subtitle is-6">{post.frontmatter.author}</p>
+                    </div>
+                </div>
+
+                <div className="content mb-1">
+                    {post.frontmatter.subtitle}
+                    <br />
+                    <time style={style.time} dateTime={style.time}>
+                        {post.frontmatter.date}
+                    </time>
+                </div>
+
+                <Link to={`/posts/${post.frontmatter.slug}`} className="button is-light is-link is-small">
+                    Continue reading
+                </Link>
+            </div>
         </div>
-        <div className="content mb-1">
-          {post.frontmatter.subtitle}
-          <br />
-          <time
-            style={style.time}
-            dateTime={style.time}>{post.frontmatter.date}</time>
-        </div>
-        <Link to={`/posts/${post.frontmatter.slug}`} className="button is-light is-link is-small">
-          Continue reading
-        </Link>
-      </div>
-    </div>
-  );
+    );
+
 }
 
 export default FeaturedPost;
