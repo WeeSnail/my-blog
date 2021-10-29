19319837a4b585f5cf3494edbb588f79f765c0b0
Anes-Marzooki <marzoukianas@gmail.com>
2021-10-29 11:09:33 +0100

    Getting the siteUrl from site's metadata

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import {Link, graphql, useStaticQuery} from 'gatsby';
+
+
+
 
 const Copyright = () => {
-  return (
-    <footer>
-      {'Copyright © '}
-      <Link color="inherit" to="/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </footer>
-  );
+    const data = useStaticQuery( graphql`
+        query{
+            site {
+                siteMetadata {
+                    siteUrl
+                }
+            }
+        }
+    `)
+
+    return (
+        <footer>
+            {'Copyright © '}
+            <Link color="inherit" to={data.site.siteMetadata.siteUrl}>
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </footer>
+    );
 };
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="p-5">
-        <nav className="level">
-          <div className="level-item has-text-centered">
-            <Copyright />
-            <a href="/rss.xml" target="_blank">RSS</a>
-          </div>
-        </nav>
-      </div>
-    </footer>
-  )
+    return (
+        <footer>
+            <div className="p-5">
+                <nav className="level">
+                    <div className="level-item has-text-centered">
+                        <Copyright />
+                        <a href="/rss.xml" target="_blank">RSS</a>
+                    </div>
+                </nav>
+            </div>
+        </footer>
+    )
 };
+
 
 export default Footer;
