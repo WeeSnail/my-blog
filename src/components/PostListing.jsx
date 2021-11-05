27b81094629dbb89005ed3ff8b2086982d70862d
Anes-Marzooki <marzoukianas@gmail.com>
2021-11-05 16:03:32 +0100

    Wrap each post in a Wrap component

--- src/components/PostListing.jsx
+++ src/components/PostListing.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Post from './Post';
 import {Link} from 'gatsby';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border: 1px solid var(--box-border);
+  box-shadow: var(--box-shadow) -0.1rem 0.1rem;
+  margin: 0.5rem;
+`;
 
 const PostListing = ({ posts }) => {
   return (
@@ -8,14 +15,14 @@ const PostListing = ({ posts }) => {
       <div className="columns is-multiline">
         {
           posts.map(post =>
-            <div key={post.id} className="column is-9">
+            <Card key={post.id} className="column is-9">
               <Post
                 title={post.frontmatter.title}
                 subtitle={post.frontmatter.subtitle}
                 date={post.frontmatter.date}
                 slug={post.frontmatter.slug}
               />
-            </div>
+            </Card>
           )
         }
         { /* Read more section: goes to paginated page */}
