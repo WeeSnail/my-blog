7f5382a368b1b371bb230abc1053b4298d3262ba
Anes-Marzooki <marzoukianas@gmail.com>
2021-11-07 15:34:04 +0100

    Avoid adding theme-color meta tag

--- utils/manifest-options.js
+++ utils/manifest-options.js
@@ -1,9 +1,10 @@
 module.exports = {
-    name: 'Anes\'s blog',
-    short_name: 'Anes\'s blog',
-    start_url: '/',
-    background_color: '#f7f0eb',
-    theme_color: '#a2466c',
-    display: 'standalone',
-    icon: `src/images/icon.png`,
+  name: 'Anes\'s blog',
+  short_name: 'Anes\'s blog',
+  start_url: '/',
+  background_color: '#f7f0eb',
+  theme_color: '#a2466c',
+  display: 'standalone',
+  icon: `src/images/icon.png`,
+  theme_color_in_head: false,
 }
