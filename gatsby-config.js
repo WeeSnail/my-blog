356e4719443348a182e643696c79c5a3fdb284bf
Anes-Marzooki <marzoukianas@gmail.com>
2021-11-08 18:13:49 +0100

    Modify metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,49 +2,49 @@ const rssOptions = require('./utils/rss-options');
 const manifestOptions = require('./utils/manifest-options');
 
 require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 
 module.exports = {
-    siteMetadata: {
-        title: "My blog",
-        description: 'My own Personal blog',
-        body: {
-            content: 'Just some SEO content'
-        },
-        siteUrl: process.env.BASE_URL,
+  siteMetadata: {
+    title: "Anes's Blog",
+    description: 'Technical Blog',
+    body: {
+      content: 'Very exceptional!!!'
     },
-    plugins: [
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-styled-components`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: manifestOptions,
-        },
-        `gatsby-plugin-offline`,
-        {
-            resolve: `gatsby-plugin-feed`,
-            options: rssOptions
-        },
-        { // File sourcing
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'content',
-                path: `${__dirname}/content/posts`,
-            }
-        },
-        { // File transformer (transform sourced files)
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                // Footnotes mode (default: true)
-                footnotes: true,
-                // GitHub Flavored Markdown mode (default: true)
-                gfm: true,
-                // Plugins configs
-                plugins: [],
-            },
-        },
-    ],
+    siteUrl: process.env.BASE_URL,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: manifestOptions,
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: rssOptions
+    },
+    { // File sourcing
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content/posts`,
+      }
+    },
+    { // File transformer (transform sourced files)
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+  ],
 };
