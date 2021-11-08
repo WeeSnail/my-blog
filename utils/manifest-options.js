42ab6adedbdd05349c4c75fa78a779dce409b3ca
Anes-Marzooki <marzoukianas@gmail.com>
2021-11-08 18:58:59 +0100

    Add custom icon and custom name

--- utils/manifest-options.js
+++ utils/manifest-options.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'Anes\'s blog',
+  name: 'Anes\'s Open Playground',
   short_name: 'Anes\'s blog',
   start_url: '/',
   background_color: '#f7f0eb',
   theme_color: '#a2466c',
   display: 'standalone',
-  icon: `src/images/icon.png`,
+  icon: `src/images/bitmap.png`,
   theme_color_in_head: false,
 }
